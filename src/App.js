7224a1747bcf17fac94fc80e146947de506f9c78
Hawk Nguyen <verynoob189@gmail.com>
2019-02-17 14:17:05 -0500

    immigrate to VSC

--- src/App.js
+++ src/App.js
@@ -1,30 +1,30 @@
 import React, { Component } from 'react';
 import './App.css';
 import "./Animate.css";
-import {NavBar} from "./containers/NavBar";
-import {MainBackground} from "./containers/MainBackground";
-import {Main} from "./containers/Main";
-import {Contact} from "./containers/Contact";
-import {ProjectPets} from "./containers/ProjectPets";
-import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import { NavBar } from "./containers/NavBar";
+import { MainBackground } from "./containers/MainBackground";
+import { Main } from "./containers/Main";
+import { Contact } from "./containers/Contact";
+import { ProjectPets } from "./containers/ProjectPets";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
       <Router basename={process.env.PUBLIC_URL}>
-          {/*<ScrollToTop>*/}
-            <div className="App">
-            
-              {console.log('public url: ', process.env.PUBLIC_URL)}
-              <Route path="/" component={NavBar}/>
-              <Route path="/" component={MainBackground}/>
-              <Switch>
-                <Route exact={true} path="/" component={Main}/>
-                <Route path="/projectpets" component={ProjectPets}/>
-              </Switch>
-              <Route path="/" component={Contact}/>
-            </div>
-          {/*</ScrollToTop>*/}
+        {/*<ScrollToTop>*/}
+        <div className="App">
+
+          {console.log('public url: ', process.env)}
+          <Route path="/" component={NavBar} />
+          <Route path="/" component={MainBackground} />
+          <Switch>
+            <Route exact={true} path="/" component={Main} />
+            <Route path="/projectpets" component={ProjectPets} />
+          </Switch>
+          <Route path="/" component={Contact} />
+        </div>
+        {/*</ScrollToTop>*/}
       </Router>
     );
   }
