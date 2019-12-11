0b1e23cef620ba2c70a93131df5302253f231c0d
Hawk Nguyen <verynoob189@gmail.com>
2019-12-11 10:53:15 -0500

    update skills

--- src/components/AboutMe/Skills.js
+++ src/components/AboutMe/Skills.js
@@ -5,22 +5,29 @@ export class Skills extends React.Component{
   render(){
     return (
       <div className="container-fluid bg-white text-center py-5">
-        <div className="heading-text pb-5 col-sm d-sm-flex align-items-center justify-content-center
-          flex-sm-column wow">
-          <h3 className="border-bottom px-2">
-            Technological Skills
-          </h3>
+        <div
+          className="heading-text pb-5 col-sm d-sm-flex align-items-center justify-content-center
+          flex-sm-column wow"
+        >
+          <h3 className="border-bottom px-2">Technological Skills</h3>
           <p>
-            I always enjoy learning new technologies. Especially, those will make your company special.
+            I always enjoy learning new technologies. Especially, those will
+            make your company special.
           </p>
         </div>
-        <div className="container text-white wow slideInUp" data-wow-duration="2s" data-wow-delay="0.5s">
+        <div
+          className="container text-white wow slideInUp"
+          data-wow-duration="2s"
+          data-wow-delay="0.5s"
+        >
           <div className="row mb-3">
             <div className="col-sm-4">
               <div className="container pl-0 pr-1 pb-1">
                 <div className="row">
                   <div className="col-sm skill-cell border rounded border-dominating-color border-width-small">
-                    <h6>Semantic HTML5 <i className="fab fa-html5"></i></h6>
+                    <h6>
+                      Semantic HTML5 <i className="fab fa-html5"></i>
+                    </h6>
                   </div>
                 </div>
               </div>
@@ -29,16 +36,20 @@ export class Skills extends React.Component{
               <div className="container pl-0 pr-1 pb-1">
                 <div className="row">
                   <div className="col-sm skill-cell rounded border border-dominating-color border-width-small">
-                    <h6>Responsive CSS3 <i className="fab fa-css3-alt"></i></h6>
+                    <h6>
+                      Responsive CSS3 <i className="fab fa-css3-alt"></i>
+                    </h6>
                   </div>
                 </div>
               </div>
-          </div>
+            </div>
             <div className="col-sm-4">
               <div className="container pl-0 pr-1 pb-1">
                 <div className="row">
                   <div className="col-sm skill-cell rounded border border-dominating-color border-width-small">
-                    <h6>Interactive JS <i className="fab fa-js-square"></i></h6>
+                    <h6>
+                      Interactive JS <i className="fab fa-js-square"></i>
+                    </h6>
                   </div>
                 </div>
               </div>
@@ -49,7 +60,9 @@ export class Skills extends React.Component{
               <div className="container pl-0 pr-1 pb-1">
                 <div className="row">
                   <div className="col-sm skill-cell rounded border border-dominating-color border-width-small">
-                    <h6>Sass <i className="fab fa-sass"></i></h6>
+                    <h6>
+                      Sass <i className="fab fa-sass"></i>
+                    </h6>
                   </div>
                 </div>
               </div>
@@ -58,16 +71,9 @@ export class Skills extends React.Component{
               <div className="container pl-0 pr-1 pb-1">
                 <div className="row">
                   <div className="col-sm skill-cell rounded border border-dominating-color border-width-small">
-                    <h6>jQuery <i className="fas fa-code"></i></h6>
-                  </div>
-                </div>
-              </div>
-          </div>
-            <div className="col-sm-3">
-              <div className="container pl-0 pr-1 pb-1">
-                <div className="row">
-                  <div className="col-sm skill-cell rounded border border-dominating-color border-width-small">
-                    <h6>Bootstrap 4 <i className="fas fa-laptop-code"></i></h6>
+                    <h6>
+                      jQuery <i className="fas fa-code"></i>
+                    </h6>
                   </div>
                 </div>
               </div>
@@ -75,8 +81,21 @@ export class Skills extends React.Component{
             <div className="col-sm-3">
               <div className="container pl-0 pr-1 pb-1">
                 <div className="row">
-                  <div className="col-sm skill-cell rounded border border-dominating-color 
-                  border-width-small d-flex">
+                  <div className="col-sm skill-cell rounded border border-dominating-color border-width-small">
+                    <h6>
+                      Bootstrap 4 <i className="fas fa-laptop-code"></i>
+                    </h6>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-sm-3">
+              <div className="container pl-0 pr-1 pb-1">
+                <div className="row">
+                  <div
+                    className="col-sm skill-cell rounded border border-dominating-color 
+                  border-width-small d-flex"
+                  >
                     {/*<h6>D3 <img className="mb-1" src={d3} alt="d3 logo" width="16" height="16"/></h6>*/}
                     <h6>Jest Testing/Debugging </h6>
                   </div>
@@ -89,7 +108,9 @@ export class Skills extends React.Component{
               <div className="container pl-0 pr-1 pb-1">
                 <div className="row">
                   <div className="col-sm skill-cell rounded border border-dominating-color border-width-small">
-                    <h6>React <i className="fab fa-react"></i></h6>
+                    <h6>
+                      React <i className="fab fa-react"></i>
+                    </h6>
                   </div>
                 </div>
               </div>
@@ -98,16 +119,20 @@ export class Skills extends React.Component{
               <div className="container pl-0 pr-1 pb-1">
                 <div className="row">
                   <div className="col-sm skill-cell rounded border border-dominating-color border-width-small">
-                    <h6>Redux <i className="fas fa-network-wired"></i></h6>
+                    <h6>
+                      React Hooks <i className="fab fa-react"></i>
+                    </h6>
                   </div>
                 </div>
               </div>
-          </div>
+            </div>
             <div className="col-sm-4">
               <div className="container pl-0 pr-1 pb-1">
                 <div className="row">
                   <div className="col-sm skill-cell rounded border border-dominating-color border-width-small">
-                    <h6>React Router <i className="fas fa-route"></i></h6>
+                    <h6>
+                      React Router <i className="fas fa-route"></i>
+                    </h6>
                   </div>
                 </div>
               </div>
@@ -118,7 +143,9 @@ export class Skills extends React.Component{
               <div className="container pl-0 pr-1 pb-1">
                 <div className="row">
                   <div className="col-sm skill-cell rounded border border-dominating-color border-width-small">
-                    <h6>ExpressJS <i className="fab fa-node-js"></i></h6>
+                    <h6>
+                      ExpressJS <i className="fab fa-node-js"></i>
+                    </h6>
                     {/*<h6>D3 <img className="mb-1" src={d3} alt="d3 logo" width="16" height="16"/></h6>*/}
                   </div>
                 </div>
@@ -128,16 +155,9 @@ export class Skills extends React.Component{
               <div className="container pl-0 pr-1 pb-1">
                 <div className="row">
                   <div className="col-sm skill-cell rounded border border-dominating-color border-width-small">
-                    <h6>Git Bash <i className="fab fa-git-square"></i></h6>
-                  </div>
-                </div>
-              </div>
-          </div>
-            <div className="col-sm-3">
-              <div className="container pl-0 pr-1 pb-1">
-                <div className="row">
-                  <div className="col-sm skill-cell rounded border border-dominating-color border-width-small">
-                    <h6>Babel <i className="fab fa-js-square"></i></h6>
+                    <h6>
+                      Git Bash <i className="fab fa-git-square"></i>
+                    </h6>
                   </div>
                 </div>
               </div>
@@ -146,7 +166,20 @@ export class Skills extends React.Component{
               <div className="container pl-0 pr-1 pb-1">
                 <div className="row">
                   <div className="col-sm skill-cell rounded border border-dominating-color border-width-small">
-                    <h6>Webpack <i className="fas fa-cube"></i></h6>
+                    <h6>
+                      Babel <i className="fab fa-js-square"></i>
+                    </h6>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-sm-3">
+              <div className="container pl-0 pr-1 pb-1">
+                <div className="row">
+                  <div className="col-sm skill-cell rounded border border-dominating-color border-width-small">
+                    <h6>
+                      Webpack <i className="fas fa-cube"></i>
+                    </h6>
                   </div>
                 </div>
               </div>
@@ -154,6 +187,6 @@ export class Skills extends React.Component{
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
